1e3459e3a57ac98dee84a65384140b2ce8f4ecdc
Riya Mhatre <riyatmhatre@gmail.com>
2025-03-10 23:55:50 +0530

    Class and Object exmaple with syntax

---
+++ Main.java
@@ -0,0 +1,38 @@
+class Riya{
+  int a;
+  String b;
+
+  //This a parametrized constructor
+  //syntax:
+  //classname(Datatype variable, Datatype variable, ...){
+    //this.variable = variable;
+  //}
+  Riya(int a, String b){
+    this.a = a;
+    this.b = b;
+  }
+
+  //This is a Method
+  //syntax:
+  //return type (eg. void, string, int, etc.) method name() {
+    //block of code
+  //}
+  void display(){
+    System.out.println(a + b);
+  }
+}
+
+public class Main{
+  public static void main(String[] args){
+    //This is an Object (Instance of class) with a constuctor
+    //syntax:
+    //classname objectname = new classname()
+    Riya r = new Riya(34, "Mthatre");
+    
+    //This is an Object without using constructor
+    //Riya r = new Riya();
+    // r.a = 34;
+    // r.b = "Mhatre";
+    r.display();
+  }
+}
